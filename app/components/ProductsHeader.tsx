86f86d1f022d85ae64957717fc1434dfa64f697e
Luciano Amaya <luciano.amayaaa@gmail.com>
2023-04-28 08:22:44 -0300

    Mobile products display functionality

--- app/components/ProductsHeader.tsx
+++ app/components/ProductsHeader.tsx
@@ -63,7 +63,7 @@ const ProductsHeader = (props: Props) => {
                     className="rotate-180 transition-transform"
                     width="15px"
                     height="15px"
-                    stroke-width="3"
+                    strokeWidth="3"
                     viewBox="0 0 24 24"
                     fill="yes"
                     xmlns="http://www.w3.org/2000/svg"
@@ -72,9 +72,9 @@ const ProductsHeader = (props: Props) => {
                     <path
                       d="M3.685 18.783l7.88-14.008a.5.5 0 01.87 0l7.88 14.008a.5.5 0 01-.617.71l-7.517-2.922a.5.5 0 00-.362 0l-7.517 2.923a.5.5 0 01-.617-.711z"
                       stroke="#000000"
-                      stroke-width="3"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="3"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     ></path>
                   </svg>
                 )}
